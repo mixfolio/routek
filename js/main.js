51de302c3bfb30fb80a4ec304c4b4b6d006de023
mixfolio <62788627+mixfolio@users.noreply.github.com>
2020-05-22 14:35:21 +0300

    ROI-counter

--- js/main.js
+++ js/main.js
@@ -25,10 +25,24 @@ $(document).ready(function () {
         $(this).toggleClass('active').next().slideToggle(200);
     });
 });
-// Roi page skillbar 
-$('.saving__bar').each(function(){
-    $(this).find('.saving__bar-bar').animate({
-        width:$(this).attr('data-percent')
-    }, 4000);
+// Roi page saving__bar 
+$(document).ready(function () {
+    $('.saving__bar').each(function(){
+        $(this).find('.saving__bar-bar').animate({
+            width:$(this).attr('data-percent')
+        }, 4000);
+    });
+});
 
+// Roi page counter
+$('.count').each(function(){
+    $(this).prop('Counter', 0).animate({
+        Counter:$(this).text()
+    },{
+        duration: 4000,
+        easing: 'swing',
+        step: function(now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
 });
